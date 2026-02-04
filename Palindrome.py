2c3bae91f1c711523b3004335fdafcafdd693884
Betsinat Wendwesen <Betsinat39@gmail.com>
2026-02-04 14:55:29 +0300

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        left = 0
+        right = (len(x)) - 1
+        for _ in range(len(x)):
+            if x[left] == x[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
