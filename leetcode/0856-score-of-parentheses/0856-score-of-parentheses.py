0faf0bea3e5efc2af7218f90094dbcdacc54408e
Betsinat Wendwesen <Betsinat39@gmail.com>
2026-03-13 15:00:01 +0300

    Add solution for Score of Parentheses

--- leetcode/0856-score-of-parentheses/0856-score-of-parentheses.py
+++ leetcode/0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,20 +1,13 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        stack = []
-        res = 0
+        stack = [0]
         for p in s:
             if p == "(":
                 stack.append(0)
             else:
-                val = stack.pop()
-                score = 1 if val == 0 else 2 * val
-                
-                if stack:
-                    stack[-1] += score
-                else:
-                    res += score
-        
-        return res
+               val = stack.pop()
+               stack[-1] += max(2*val , 1)
+        return stack[0]
 
        
 
